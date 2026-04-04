6a48579ec932b1e4539e5226925b20d3e15ef3d0
Yashveer Singh <singhyash9631@outlook.com>
2026-04-04 15:08:31 +0530

    Day3

--- src/config/db.js
+++ src/config/db.js
@@ -9,7 +9,7 @@ function connectToDB() {
             console.log("server is connected to DB")
         })
         .catch(err => {
-            console.log("Error connecting to DB")
+            console.log("Error connecting to DB:", err.message)
             process.exit(1)
         })
 
